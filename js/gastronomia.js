67b67323c74a8671644c5413bcf58ed6b2056604
Jyoan Figueroa <84805967+JyoanF@users.noreply.github.com>
2022-08-06 09:00:09 -0500

    v1.0

---
+++ js/gastronomia.js
@@ -0,0 +1,34 @@
+document.addEventListener('onload', mostrarCategorias());
+
+function mostrarCategorias(){
+    const xhttp = new XMLHttpRequest();
+    xhttp.open('GET', './json/gastronomia.json',true);
+
+    xhttp.send();
+
+    xhttp.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status==200){
+            let menu_container=document.querySelector('#contenedor-categorias');
+            menu_container.innerHTML='';
+
+            let categorias = JSON.parse(this.responseText);
+
+            for(let item of categorias){
+                menu_container.innerHTML += `<a href="/gresult.html" name="${item.id}">
+                <div class="tk_section_card text-center">
+                    <div>
+                        <img src="${item.dir_background}" class="tk_cat_bg">
+                    </div>
+                    <div class="bg_dark"></div>
+                    <div class="tk_card_info">
+                        <div>
+                            <img src="${item.logo}" alt="" width="80px"/>
+                            <h5>${item.nombre}</h5>
+                        </div>
+                    </div>
+                </div>
+            </a>`
+            }
+        }
+    }
+}
